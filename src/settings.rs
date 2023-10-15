d07645ac19e2d76fdadcde4dd02b5fc39dbf8337
Levi Araújo <leviprudenciont@gmail.com>
2023-10-15 12:36:30 -0300

    feat: create database connection

---
+++ src/settings.rs
@@ -0,0 +1,33 @@
+use config::{self, Config, ConfigError, File, FileFormat};
+
+#[derive(serde::Deserialize)]
+pub struct Settings {
+    pub database: DatabaseSettings,
+    pub application_port: u16,
+}
+#[derive(serde::Deserialize)]
+pub struct DatabaseSettings {
+    pub username: String,
+    pub password: String,
+    pub port: u16,
+    pub host: String,
+    pub database_name: String,
+}
+
+impl DatabaseSettings {
+    pub fn connection_string(&self) -> String {
+        format!(
+            "postgres://{}:{}@{}:{}/{}",
+            self.username, self.password, self.host, self.port, self.database_name
+        )
+    }
+}
+
+pub fn get_settings() -> Result<Settings, ConfigError> {
+    let config = Config::builder()
+        .add_source(File::new("settings", FileFormat::Yaml))
+        .build()?;
+
+    let settings = config.try_deserialize::<Settings>()?;
+    Ok(settings)
+}
